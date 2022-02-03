02447836bd7b57c0becce68e3ff9afb66dd68c3c
ZCube <zcube@zcube.kr>
2022-02-04 00:35:54 +0900

    fix dependencies for arm64.

--- components/docker-up/dependencies.sh
+++ components/docker-up/dependencies.sh
@@ -9,6 +9,6 @@ DOCKER_VERSION=19.03.15
 DOCKER_COMPOSE_VERSION=2.2.3
 SLIRP4NETNS_VERSION=v1.1.12
 
-echo curl -o docker.tgz      -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/docker-${DOCKER_VERSION}.tgz
-echo curl -o docker-compose  -fsSL https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-$(uname -m)
-echo curl -o slirp4netns     -fsSL https://github.com/rootless-containers/slirp4netns/releases/download/${SLIRP4NETNS_VERSION}/slirp4netns-$(uname -m)
+curl -o docker.tgz      -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/docker-${DOCKER_VERSION}.tgz
+curl -o docker-compose  -fsSL https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-$(uname -m)
+curl -o slirp4netns     -fsSL https://github.com/rootless-containers/slirp4netns/releases/download/${SLIRP4NETNS_VERSION}/slirp4netns-$(uname -m)
